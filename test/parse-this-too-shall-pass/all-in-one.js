d4e2ebca9a60ad71a7fd3ac99998f72429537b76
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-10-08 16:03:36 -0700

    added all-in-one test

---
+++ test/parse-this-too-shall-pass/all-in-one.js
@@ -0,0 +1,23 @@
+{
+    unquoted_key: "keys must be quoted",
+    a1: ["extra comma",],
+    a2: ["double extra comma",,],
+    // single line comment
+    "Illegal expression": 1 + 2,
+    "Illegal invocation": alert(),
+    "Numbers cannot have leading zeroes": 013,
+    "Numbers cannot be hex": 0x14,
+    a3: [
+        "Illegal backslash escape: \x15",
+        "Illegal backslash escape: \017",
+        [[[[[[[[[[[[[[[[[[[["Too deep"]]]]]]]]]]]]]]]]]]]],
+        "Bad value", truth,
+        'single quote',
+        /* multi-line comment */
+        "	tab	character	in	string	",
+        "tab\   character\   in\  string\  ",
+        "line\
+        break"
+    ],
+    "Extra comma": true,
+}
